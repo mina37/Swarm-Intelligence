165d727cd09917dd20c00426fa5c49673e8eac3c
mina37 <minasherif7@hotmail.com>
2017-03-10 16:02:12 +0200

    ultrasonic main code

---
+++ UltraSonic_V1.ino
@@ -0,0 +1,39 @@
+unsigned long echo = 0;
+int ultraSoundSignal = 7; // Ultrasound signal pin
+int ultraSonicEcho = 6;
+unsigned long ultrasoundValue = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ultraSoundSignal,OUTPUT);
+  pinMode(ultraSonicEcho,INPUT);
+}
+
+unsigned long ping()
+{ 
+  //pinMode(ultraSoundSignal, OUTPUT); // Switch signalpin to output
+  digitalWrite(ultraSoundSignal, LOW); // Send low pulse 
+  delayMicroseconds(2); // Wait for 2 microseconds
+  digitalWrite(ultraSoundSignal, HIGH); // Send high pulse
+  delayMicroseconds(5); // Wait for 5 microseconds
+  digitalWrite(ultraSoundSignal, LOW); // Holdoff
+  //pinMode(ultraSoundSignal, INPUT); // Switch signalpin to input
+  digitalWrite(ultraSonicEcho, HIGH); // Turn on pullup resistor
+  // please note that pulseIn has a 1sec timeout, which may
+  // not be desirable. Depending on your sensor specs, you
+  // can likely bound the time like this -- marcmerlin
+  // echo = pulseIn(ultraSoundSignal, HIGH, 38000)
+  echo = pulseIn(ultraSonicEcho, HIGH); //Listen for echo
+  ultrasoundValue = (echo / 58.138); //convert to CM then to inches
+  return ultrasoundValue;
+}
+
+void loop()
+{
+  unsigned int x = 0;
+  x = ping();
+  if(x!=0)
+    Serial.println(x);
+  delay(400); //delay 1/4 seconds.
+}
