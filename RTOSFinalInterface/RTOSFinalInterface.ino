626aa3fbde019899089c91ac431fdd81d47ee797
mina37 <minasherif7@hotmail.com>
2017-06-18 15:10:50 +0200

    Full code
    this version is missing some optimization and the RF

---
+++ RTOSFinalInterface/RTOSFinalInterface.ino
@@ -0,0 +1,150 @@
+#include <Arduino_FreeRTOS.h>
+#include "task.h"
+#include "macros.h"
+long LineDetectorCounter_1 = 0 ;
+long LineDetectorCounter_2 = 0 ;
+
+void LineDetectorFunction_1()
+{
+ LineDetectorCounter_1++;
+ }
+void LineDetectorFunction_2()
+{
+ LineDetectorCounter_2++;
+ }
+
+
+unsigned long ultrasonic(char i)
+{
+  unsigned long ultrasoundValue;
+    pinMode(ultraSoundSignal + i, OUTPUT); //set the signal pin to OUTPUT
+    pinMode(ultraSonicEcho + i, INPUT);    //sets the echo pin to INPUT
+    digitalWrite(ultraSonicEcho + i, HIGH); // Turn on pullup resistor
+  unsigned long echo = 0;                  //duration of the echo pulse
+      while(echo ==0){
+      digitalWrite(ultraSoundSignal + i, LOW); // Send low pulse
+      delayMicroseconds(2); // Wait for 2 microseconds
+      digitalWrite(ultraSoundSignal + i, HIGH); // Send high pulse
+      delayMicroseconds(5); // Wait for 5 microseconds
+      digitalWrite(ultraSoundSignal + i, LOW); // Holdoff
+      // please note that pulseIn has a 1sec timeout, which may
+      // not be desirable. Depending on your sensor specs, you
+      // can likely bound the time like this -- marcmerlin
+      echo = pulseIn(ultraSonicEcho + i, HIGH); //Listen for echo
+                   //enables the scheduler
+      //gets the average of last 10 non zero readings
+          //ultrasound value
+        //xQueueReceive( UltraSonicQ, &( ultrasoundValue ), (TickType_t)0); //discards oldest of the the last 10 readings
+        ultrasoundValue = (echo / 58.138); //convert to CM
+      }
+      if (ultrasoundValue < 300)
+        {
+          return ultrasoundValue;
+        }
+  else
+  return 0;
+}
+
+
+unsigned long colorSensor(char color)
+{
+  unsigned long counter = 0; //gets input from sensor frequency generator
+  unsigned long countR = 0; //accumilates reading over 8 times to get average
+  unsigned long count = 0; //saves the final average reading
+
+  //gets 8 reading for each color
+  for (char i = 0; i < 8; i++) {
+    if (color == 1) {
+      digitalWrite(s2, LOW);
+      digitalWrite(s3, LOW); 
+    }
+    else if (color == 0) {
+      digitalWrite(s2, LOW);
+      digitalWrite(s3, HIGH);
+    }
+    else if (color == 2) {
+
+
+      digitalWrite(s2, HIGH);
+      digitalWrite(s3, HIGH);
+    }
+    else
+    {
+      digitalWrite(s2, HIGH);
+      digitalWrite(s3, LOW);
+    }
+
+    counter = 0;  //resets counter
+    //stops RTOS interrupt
+    counter = pulseIn(out, LOW);  //reads the pulse width which is inversely proportional to the frequency
+    //so the less the pulse width the more the frequency and the more the intensity
+
+    countR = countR + counter;
+
+  }
+  //Serial.print("red=");
+  
+  
+  ///////////////////////////
+ // countR = countR >> 7;
+  //count = (unsigned char) countR;
+//////////////////
+
+  count = countR>>1;
+  //Serial.println(countR,DEC);
+  return count;
+}
+
+
+int counter = 0;
+void MainTask(void * param)
+{
+  for(;;)
+  {
+    for(char i =0;i<3;i++)
+  {
+    Serial.print("ultrasonic: ");
+    Serial.println(i,DEC);
+    Serial.println(ultrasonic(i));
+  }
+  //for testing 
+Serial.print("COUNTER_1 = ");
+Serial.println(RPMCounter_1);
+Serial.print("COUNTER_2 = ");
+Serial.println(RPMCounter_2);
+for(char i = 0;i<4;i++)
+{
+  Serial.print("ColorSensor: ");
+  Serial.println(i,DEC);
+  Serial.println(colorSensor(i),DEC);
+
+}
+MotorDriverLeft(200,1);
+MotorDriverRight(200,1);
+vTaskDelay(200);
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode( LineDetectorPin_1 , INPUT );
+pinMode( LineDetectorPin_2 , INPUT );
+pinMode(LineDetectorPin_1 , INPUT_PULLUP);
+pinMode(LineDetectorPin_2 , INPUT_PULLUP);
+attachInterrupt ( digitalPinToInterrupt(LineDetectorPin_1) , LineDetectorFunction_1 , FALLING);
+attachInterrupt ( digitalPinToInterrupt(LineDetectorPin_2) , LineDetectorFunction_2 , RISING);
+
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
+  pinMode(out, INPUT);
+  Serial.begin(9600); //initialises the serial for debugging
+  TaskHandle_t  First_handle; //task handles and it is redundant and can be replaced with NULL as long as it is neede
+  xTaskCreate(MainTask, "Task2",128, NULL,1, &First_handle); //creating main task
+  //starting the scheduler
+  vTaskStartScheduler();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ RTOSFinalInterface/macros.h
@@ -0,0 +1,38 @@
+//conststants
+#define SWITCHTASKSTACKSIZE        128  //Stack Size
+#define MotorDriverLeft( pwm , dir){    digitalWrite( motorL1, LOW );    digitalWrite( motorR1, LOW );   switch (dir)  { case 1: analogWrite(motorL1, pwm);  break; case 0: analogWrite(motorR1, pwm);   break;  }}
+#define MotorDriverRight( pwm , dir){    digitalWrite( motorL2, LOW );    digitalWrite( motorR2, LOW );   switch (dir)  { case 1: analogWrite(motorL2, pwm);  break; case 0: analogWrite(motorR2, pwm);   break;  }}
+//1 tick = 17.5 ms
+
+
+#define ROTATION_90_TIME 2 * TICKS_PER_SECOND
+
+
+
+//pins used
+#define ultraSonicEcho  A3 //Ultrasound signal pin
+#define ultraSoundSignal  A0 // Ultrasound signal pin
+//#define s0  4
+//#define s1  3
+#define s2  10
+#define s3  12
+#define out  4
+#define motorL1 11
+#define motorR1 5
+#define motorL2 6
+#define motorR2 9
+
+#define LineDetectorPin_1 2
+#define LineDetectorPin_2 3 
+#define RPMCounter_1 LineDetectorCounter_1
+#define RPMCounter_2 LineDetectorCounter_2
+
+
+
+//macros and enums
+#define BLUE 0
+#define RED 1
+#define Green 2
+#define WHITE 3
+
+
