7ed0f0419b1fbb35076f198ab8c71fac5e3a9f68
mina37 <minasherif7@hotmail.com>
2017-07-01 00:42:09 +0300

    Pins updated

--- RTOSFinalInterface_3.0/motordriver.h
+++ RTOSFinalInterface_3.0/motordriver.h
@@ -2,8 +2,9 @@
 #define motor    
 
 
-#define MotorDriverLeft( pwm , dir){    switch (dir)  { case 1: analogWrite(motorL1, 0); analogWrite(motorR1,pwm);  break; case 0: analogWrite(motorL1,  pwm); analogWrite(motorR1,0);   break;  }}
-#define MotorDriverRight( pwm , dir){    switch (dir)  { case 1: analogWrite(motorL2, 255 - pwm); analogWrite(motorR2,255);  break; case 0: analogWrite(motorL2, pwm); analogWrite(motorR2,0);   break;  }}
+#define MotorDriverLeft(pwm , dir) {    switch (dir)  { case 1: analogWrite(motorL1, pwm);digitalWrite( motorR1, LOW );  break;  case 0: analogWrite(motorR1, pwm); digitalWrite( motorL1, LOW ); break;}}
+#define MotorDriverRight(pwm , dir){   switch (dir)  { case 1: analogWrite(motorL2, pwm); digitalWrite( motorR2, LOW ); break;  case 0: analogWrite(motorR2, pwm); digitalWrite( motorL2, LOW ); break;}}
+
 
 #define BreakMotorLeft() digitalWrite( motorL1, HIGH );    digitalWrite( motorR1, HIGH )
 #define BreakMotorRight() digitalWrite( motorL2, HIGH );    digitalWrite( motorR2, HIGH )
@@ -12,9 +13,9 @@
 #define StopMotorRight() digitalWrite( motorL2, LOW );    digitalWrite( motorR2, LOW )
 
 #define motorL1 5 //11
-#define motorR1 A6 //5
+#define motorR1 10 //5
 #define motorL2 6
-#define motorR2 A7 //9
+#define motorR2 9 //9
 
 
 void init_motor_driver()
