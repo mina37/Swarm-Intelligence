530a0e2dee506cd7d52abc9cf1c0f0e83ac86987
Michael-Adel <mike-adel@hotmail.com>
2017-07-09 00:57:57 +0200

    Add files via upload

---
+++ forward_sherry.ino
@@ -0,0 +1,24 @@
+#include "BlueSM.h"
+#include "RF24.h"
+#include <SPI.h>
+
+void setup() 
+{
+ init_motor_driver();
+ init_line_tracker();
+}
+
+void loop()
+{
+ 
+ unsigned long prevRight = 0;
+ unsigned long prevLeft = 0;
+ MotorDriverLeft(160,1);
+ 
+
+ signed int e = (LineDetectorCounterRight - prevRight) - (LineDetectorCounterLeft - prevLeft);
+ MotorDriverRight(160-(30*e),1)
+ prevRight = LineDetectorCounterRight;
+ prevLeft = LineDetectorCounterLeft;
+
+}
